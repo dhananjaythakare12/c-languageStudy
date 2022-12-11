35ced35ae06b41aa5662d44919f43933192ddf43
dhananjaythakare12 <thakareshravan21@gmail.com>
2022-12-11 12:28:36 +0530

    initial commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int i, j;
+    for (i = 10; i >= 1; i--)
+        ;
+    {
+        for (j = 1; j = i; j++)
+            ;
+        {
+            printf("1");
+        }
+        printf("\n");
+    }
+    return 0;
+    c = 0 ?
+}
